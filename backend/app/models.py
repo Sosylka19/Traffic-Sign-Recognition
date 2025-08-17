bcaa6a3a65ca43d73f8ba57467c68521880fdd5b
Sosylka19 <sashasasha192021@gmail.com>
2025-08-17 22:09:00 +0300

    upd:add backend/database

---
+++ backend/app/models.py
@@ -0,0 +1,9 @@
+from typing import Optional
+from sqlmodel import Field, SQLModel
+
+class Users(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    email: str = Field(index=True, unique=True)
+    password: str
+
+    
